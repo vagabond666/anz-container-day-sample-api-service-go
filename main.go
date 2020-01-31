a852c7dcf13af138cdc2f7aa080c08e6692ca249
vagabond666 <52558111+vagabond666@users.noreply.github.com>
2020-01-31 12:39:05 +1100

    Update main.go

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "I have altered the text, pray I don't change it further"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
